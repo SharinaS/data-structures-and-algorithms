c8a228c2457626cbc7027361487e42d8c9e8a7e6
Sharina Stubbs <sharinastubbs@gmail.com>
2019-08-28 14:05:39 -0700

    challenges12 finished

--- code-challenges/nestedArrays/challenges-12.test.js
+++ code-challenges/nestedArrays/challenges-12.test.js
@@ -111,6 +111,7 @@ const battleship = (board, row, col) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
 
+
 Write a function named calculateProduct that takes in a two-dimensional array of numbers, multiplies all of the numbers in each array, and returns the final product. This function should work for any number of inner arrays.
 
 For example, the following input returns a product of 720: [[1,2], [3,4], [5,6]]
