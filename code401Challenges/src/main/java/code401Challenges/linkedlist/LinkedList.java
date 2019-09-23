8c4104b7e7d2a19e3025daa47ae1d991923ac382
Sharina Stubbs <sharinastubbs@gmail.com>
2019-09-23 15:21:56 -0700

    added in notes and comments to code during code review for learning purposes

--- code401Challenges/src/main/java/code401Challenges/linkedlist/LinkedList.java
+++ code401Challenges/src/main/java/code401Challenges/linkedlist/LinkedList.java
@@ -9,14 +9,22 @@ public class LinkedList {
 
     // === Insert a new node into the front of the lit ===
     public void insertAtHead(int value){
+        /*
+        // Alternate way of writing the active code:
+        Node newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        */
+
         // create new object
         Node node = new Node();
         node.value = value;
 
-        // if there's no other nodes:
+        // if this list's head is null, set the head to be the new node
         if(head == null) {
             head = node;
         } else {
+            // otherwise, set the head to be the new node, and set the new node's next to be the old head.
             Node temp = head;
             head = node;
             head.next = temp;
@@ -52,3 +60,46 @@ public class LinkedList {
         return false;
     }
 }
+
+/*
+GENERICS:
+Making a class into a generic class that can take different types is not hard. Add inot its definition that it can
+take different types of data. Use angle brackets. Refer to a parameter of whatever type this linked list is of.
+Type parameter T, once specified can now be used anywhere you want to indicate what the correct type is. Use T for text or
+E for element. Replace the ints as indicated with T, or E.
+
+public class LinkedList<T> {
+    Node<T> head;
+
+    public void insertTail(T value) {
+        Node<T> newNode = new Node<>(value); // Java will figure out what needs to be in the empty diamond
+        newNode.next = null;
+    }
+
+}
+
+Class Node<E> {
+    E value;
+    Node<E> next;
+
+    Node(E value){
+
+    }
+}
+
+
+// testing must also change
+public class LinkedListTest {
+    LinkedList<Integer> list;
+
+    @Test
+    public void testHead() {
+        assertEquals(
+            "Head should be 1",
+            1,
+            list.head.value.intValue()) <--- takes an integer and turn it into an int.
+        );
+    }
+
+When using objects instead of primitives, use .equal, not ==
+ */
