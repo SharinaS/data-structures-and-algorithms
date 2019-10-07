c12da58a5123705bc6b9cd3c1093ce1b08870dc3
Sharina Stubbs <sharinastubbs@gmail.com>
2019-10-07 11:14:33 -0700

    corrected type issues in tree class, rewrote code for adding a node to binary search tree, added another test for binary search tree

--- code401Challenges/src/main/java/code401Challenges/tree/BinarySearchTree.java
+++ code401Challenges/src/main/java/code401Challenges/tree/BinarySearchTree.java
@@ -3,43 +3,49 @@ package code401Challenges.tree;
 import code401Challenges.BinarySearch;
 
 // some logic referenced from: https://www.youtube.com/watch?v=wmS-ClgZ1w8&t=584s
+// Main resource: https://www.baeldung.com/java-binary-tree
 
 
 public class BinarySearchTree {
 
-    private Node root;
+    private Node <Integer> root;
 
 
     public BinarySearchTree() {
         root = null;
     }
 
+
     // adds a node to the binary search tree
-    public void add(int value) {
-        addingHelp(root, value);
+    public void add(Integer value) {
+        root = addingHelp(root, value);
     }
 
-    // helper method for add method
-    private Node addingHelp(Node root, int value) {
-        if(root == null){
-            root = new Node (value);
+    // recursive method to do the insertion
+    private Node <Integer> addingHelp(Node <Integer> curr, Integer value) {
+//        if (value < curr.value) {
+//            if (curr.left )
+//        }
 
-        } else if(root.value > value){ // <-----------------
-            // add the value of the left child
-            root.left = addingHelp(root.left, value);
-        } else {
-            // add the value of the right child
-            root.right = addingHelp(root.right, value);
+        if (curr == null) {
+            return new Node<>(value);
         }
-        return root;
+        if (value < curr.value) {
+            curr.left = addingHelp(curr.left, value);
+        } else if (value > curr.value) {
+            curr.right = addingHelp(curr.right, value);
+        } else {
+            return curr;
+        }
+        return curr;
     }
 
 
     // checks if the value is in the tree at least once
-    public Boolean contains(int value) {
-        Node currNode = root;
+    public boolean contains(int value) {
+        Node <Integer> currNode = root;
         while (!currNode.value.equals(value)) {
-            if (value < currNode.value) { // <-----------------
+            if (value < currNode.value) {
                 currNode = currNode.left;
             } else {
                 currNode = currNode.right;
@@ -52,4 +58,14 @@ public class BinarySearchTree {
         return true;
     }
 
+    @Override
+    public String toString() {
+        return "BinarySearchTree{" +
+                "root=" + root +
+                '}';
+    }
+
+    public Node<Integer> getRoot() {
+        return root;
+    }
 }
