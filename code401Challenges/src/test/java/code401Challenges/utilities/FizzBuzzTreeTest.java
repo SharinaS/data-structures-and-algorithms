a7824ba59805a0b4ae4128657a4c3bac192adb5d
Sharina Stubbs <sharinastubbs@gmail.com>
2019-10-08 15:06:59 -0700

    updated readme, added tests to complete code challenge

--- code401Challenges/src/test/java/code401Challenges/utilities/FizzBuzzTreeTest.java
+++ code401Challenges/src/test/java/code401Challenges/utilities/FizzBuzzTreeTest.java
@@ -20,13 +20,49 @@ public class FizzBuzzTreeTest {
         root.left.right = new Node ("7");
         root.right = new Node("3");
 
+
         FizzBuzzTree testTree = new FizzBuzzTree(root);
         testTree.fizzbuzzRecursion();
         assertEquals(
                 "Fizz",
                 testTree.root.right.value
-
         );
     }
+
+    // check that a number comes back if not divisible by 3 or 5
+    @Test
+    public void testFizzBuzz2() {
+        Node root = new Node("1");
+        root.left = new Node("2");
+        root.left.left = new Node ("6");
+        root.left.right = new Node ("7");
+        root.right = new Node("3");
+
+        FizzBuzzTree testTree = new FizzBuzzTree(root);
+        testTree.fizzbuzzRecursion();
+        assertEquals(
+                "7",
+                testTree.root.left.right.value
+        );
+    }
+
+    // check that BizzFuzz returns from 15 value
+    @Test
+    public void testFizzBuzz3() {
+        Node root = new Node("1");
+        root.left = new Node("2");
+        root.left.left = new Node ("6");
+        root.left.right = new Node ("7");
+        root.right = new Node("3");
+        root.right.right = new Node("15");
+
+        FizzBuzzTree testTree = new FizzBuzzTree(root);
+        testTree.fizzbuzzRecursion();
+        assertEquals(
+                "BizzFuzz",
+                testTree.root.right.right.value
+        );
+    }
+
 
 }
